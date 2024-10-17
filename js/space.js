4fea03e4d86b8d375eaead740694cce0558851f7
antonelladutruel <antonelladutruel@gmail.com>
2024-10-17 12:16:27 -0300

    Bootstrap/Styles

--- js/space.js
+++ js/space.js
@@ -5,24 +5,32 @@ const container = document.getElementById("contenedor");
 btn.addEventListener("click", () => {
     const search = input.value;
 
+    container.innerHTML = '';  
+
     fetch(`https://images-api.nasa.gov/search?q=${search}`)
     .then(response => response.json())
     .then(({ collection: { items } }) => {
         items.forEach(item => {
-          const { data, links } = item; 
-    
-          data.forEach(({ title, description }) => {
-            console.log(`titulo: ${title}`); 
-            console.log(`descripción: ${description}`)
-          });
-        
-        links.forEach(({ href }) => {
-            console.log(`links: ${href}`)
-        })
-        
-        });
+            const { data, links } = item;
+
+            data.forEach(({ title, description, date_created }) => {
+                let imgSrc = links[0].href; 
+
+                container.innerHTML += `
+                <div class="col-sm-4 mb-3">
+                    <div class="card" style="width: 22rem;">
+                        <img src="${imgSrc}" class="card-img-top" alt="${title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">${description}</p>
+                        </div>
+<p class="text-secondary small text-start ps-3">${date_created}</p>
+                    </div>
+                </div>`;
+            });
         });
     })
     .catch(error => {
         console.error('Error:', error);
     });
+});
